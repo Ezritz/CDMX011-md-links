a53e16a08c947db5aec67e4bb9278ceb8975c0df
ezritz <lidia.edith.escoto@gmail.com>
2021-10-07 18:13:02 -0500

    Fix regex

--- prueba.js
+++ prueba.js
@@ -7,7 +7,7 @@ const chalk = require('chalk');
 
 // Recursive function
 const directory = (route, subdir = '') => { // 
-  console.log('in: '+ route);
+  // console.log('in: '+ route);
   let arrFiles = [];
 
   data = fs.readdirSync(route + '/' + subdir, {withFileTypes: true}); // read directories synchronously 
@@ -19,19 +19,19 @@ const directory = (route, subdir = '') => { //
     }
     if (item.isDirectory()) {
       // console.log('isDir: ' + route + '/'+item.name);
-      arrFiles = arrFiles.concat(directory(route, subdir + '/' + item.name));
+      arrFiles = arrFiles.concat(directory(route, subdir + '/' + item.name));;
     } else if (path.extname(item.name) === '.md') {
-      console.log(chalk.yellow('isFile: ')+ chalk.green(item.name));
+      // console.log(chalk.yellow('isFile: ')+ chalk.green(item.name));
       arrFiles.push(subdir + '/' + item.name);
       // console.log(item.name);
     }
   })
   
-  // console.log('out: '+ arrFiles);
+  // console.log('Result: '+ chalk.inverse(arrFiles));
   return arrFiles;
 };
 
-console.log(directory(route));
+// console.log(directory(route));
 
 //CLI
 
@@ -49,20 +49,22 @@ inquirer.prompt([{
     console.log('File: ' + answers.path);
 
     // read file
-    const regex = /((\w+:\/\/\S+)|(\w+[\.:]\w+\S+))[^\s,\.]/ig;
+    const regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+    // const regex = /\[[\s\S]*?\]\([\s\S]*?\)/g;
+    // const regex = /((\w+:\/\/\S+)|(\w+[\.:]\w+\S+))[^\s,\.]/ig
+    // const regex = /((\w+:\/\/\w.*)|(\w+[\.:]\w+\S..))[^\s,\.]/ig;
+    // const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
     const join = path.join(route, answers.path)
     fs.readFile(join, 'utf-8', (error, data) => {
+      let resultLinks = data.match(regex);
       if (error) {
         console.log(error);
-      } else if (regex.test(data)) {
-        console.log('content:\n' + data);
+      } 
+      if (regex.test(data)) {
+        console.log(resultLinks);
       }
     });
   })
   .catch(error => {
     console.log(error);
   })
-
-
-
-   
--- src/module/prueba.md
+++ src/module/prueba.md
@@ -1,4 +1,4 @@
 [`Google`](www.google.com)
-[`Youtube`](www.youtube.com)
+[`stackoverflow`](https://stackoverflow.com/questions/43892252/how-do-i-restrict-my-last-character-using-regex)
 [`Node`](https://nodejs.org/en/)
 aqui prueba
